dd080390fb8d70ac6c04d075ff1cd0478ef37d26
Holiv <mrheltonso@gmail.com>
2022-12-18 20:07:46 -0300

    adding functionalities (Utilities.cs)

--- BettanyShopHRM/Program.cs
+++ BettanyShopHRM/Program.cs
@@ -1,33 +1,98 @@
 ﻿using System.Text;
+using BettanyShopHRM;
 using BettanyShopHRM.HR;
 
-Console.WriteLine("Creating an employee");
-Console.WriteLine("----------\n");
+List<Employee> employees = new List<Employee>();
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("***********************************");
+Console.WriteLine("* Bettany's Pie Shop Employee App *");
+Console.WriteLine("***********************************");
+Console.ForegroundColor = ConsoleColor.White;
+
+int userSelection;
+
+Console.ForegroundColor = ConsoleColor.Blue;
+
+Utilities.CheckForExistingEmployeeFile();
+
+do
+{
+    Console.ForegroundColor = ConsoleColor.Cyan;
+    Console.WriteLine($"Loaded {employees.Count} employe(s)\n\n");
+
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("********************");
+    Console.WriteLine("* Select an Action *");
+    Console.WriteLine("********************");
+    Console.ResetColor();
+
+    Console.WriteLine("1: Register employee");
+    Console.WriteLine("2: View all employees");
+    Console.WriteLine("3: Save data");
+    Console.WriteLine("4: Load data");
+    Console.WriteLine("9: Quit application");
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Your selection: ");
+    Console.ResetColor();
+
+    userSelection = Int32.Parse(Console.ReadLine());
+
+    switch (userSelection)
+    {
+        case 1:
+            Utilities.RegisterEmployee(employees);
+            break;
+        case 2:
+            Utilities.ViewAllEmployees(employees);
+            break;
+        case 3:
+            Utilities.SaveEmployee(employees);
+            break;
+        case 4:
+            Utilities.LoadEmployees(employees);
+            break;
+        case 9: break;
+    }
+} while (userSelection != 9);
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("************************************");
+Console.WriteLine("* Thanks for using the application *");
+Console.WriteLine("************************************");
+Console.ResetColor();
+
+
+
+// ---- ↓↓↓↓↓↓↓↓↓↓↓↓ ALL CODE BELOW WAS USED DURING THE COURSE TO INTRODUCE THE LANGUAGE FEATURES UNTIL FINALLY CREATING THE REAL APPLICATION
+
+/*Console.WriteLine("Creating an employee");
+Console.WriteLine("----------\n");*/
 
 // ---------- ↓↓↓↓ INSTANTIATING OBJECTS USING ITS RESPECTIVE TYPES ↓↓↓↓
-    StoreManager marcos = new StoreManager("Marcos", "Smith", "marc@snowball.be", new DateTime(1992, 1, 24), 25);
-    Developer july = new Developer("July", "Belly", "belly@jj.com", new DateTime(1981, 2, 5), 40);
+    /*StoreManager marcos = new StoreManager("Marcos", "Smith", "marc@snowball.be", new DateTime(1992, 1, 24), 25);
+    Developer july = new Developer("July", "Belly", "belly@jj.com", new DateTime(1981, 2, 5), 40);*/
 // ---------- ↑↑↑↑ INSTANTIATING OBJECTS USING ITS RESPECTIVE TYPES ↑↑↑↑
 
 
 // ---------- ↓↓↓↓ INSTANTIATING OBJECTS USING BASE TYPE REFFERENCE ↓↓↓↓
-    Employee bethany = new StoreManager("Bethany", "Smith", "bethany@snowball.be", new DateTime(1992, 1, 24), 25);
+   /* Employee bethany = new StoreManager("Bethany", "Smith", "bethany@snowball.be", new DateTime(1992, 1, 24), 25);
     Employee george = new Developer("George", "Lucas", "george@lucas.com", new DateTime(1981, 2, 5), 40);
     Employee mary = new Researcher("Mary", "Brave", "mb@studio.com", new DateTime(1992, 1, 24), 30);
     Employee bobJunior = new JuniorResearcher("Bob", "Junior", "bobjunior@bob.com", new DateTime(2000, 3, 20), null);
-    Employee kevin = new StoreManager("Kevin", "Bacon", "kavinbacon@gg.com", new DateTime(1987, 2, 25), 80);
+    Employee kevin = new StoreManager("Kevin", "Bacon", "kavinbacon@gg.com", new DateTime(1987, 2, 25), 80);*/
 // ---------- ↑↑↑↑ INSTANTIATING OBJECTS USING BASE TYPE REFFERENCE ↑↑↑↑
 
 
 // ---------- ↓↓↓↓ INSTANTIATING OBJECTS USING INTERFACE REFFERENCE ↓↓↓↓
-    IEmployee bruno = new Researcher("Bruno", "Gay", "bg@falabosta.com", new DateTime(1992, 1, 24), 30);
+    /*IEmployee bruno = new Researcher("Bruno", "Gay", "bg@falabosta.com", new DateTime(1992, 1, 24), 30);
     IEmployee fabiano = new JuniorResearcher("Fabiano", "Marmita", "paumandado@doido.com", new DateTime(2000, 3, 20), null);
-    IEmployee cadu = new StoreManager("Carlos", "Eduardo", "paidobeni@nfl.com", new DateTime(1987, 2, 25), 80);
+    IEmployee cadu = new StoreManager("Carlos", "Eduardo", "paidobeni@nfl.com", new DateTime(1987, 2, 25), 80);*/
 // ---------- ↑↑↑↑ INSTANTIATING OBJECTS USING INTERFACE REFFERENCE ↑↑↑↑
 
 
 // ---------- ↓↓↓↓ CREATING USERS TO WORK WITH INHERITANCE ↓↓↓↓
-    Manager helton = new Manager("Helton", "Oliveira", "helton@p30.dev", new DateTime(1992, 1, 24), 100);
+    /*Manager helton = new Manager("Helton", "Oliveira", "helton@p30.dev", new DateTime(1992, 1, 24), 100);
     helton.AttendManagementMeeting();
 
     Developer thomas = new Developer("Thomas", "Oliveira", "thominhas@p30.dev", new DateTime(2022, 7, 12), 200);
@@ -36,25 +101,25 @@ Console.WriteLine("----------\n");
 
     Researcher laiz = new Researcher("Laiz", "Alvarenga", "laiztaste@pie.com", new DateTime(1994, 6, 7), 150);
     Console.WriteLine(laiz.NumberOfPieTasteInvented);
-    laiz.ResearchNewPieTaste(44);
+    laiz.ResearchNewPieTaste(44);*/
 // ---------- ↑↑↑↑ CREATING USERS TO WORK WITH INHERITANCE ↑↑↑↑
 
 
 // ---------- ↓↓↓↓ CREATING A USER TO APPLY POLIMORPHISM IN ONE OF THE METHODS (USING BASE TYPE REFFERENCE) ↓↓↓↓
-    Employee jake = new Manager("Jake", "Blake", "jk@jk.com", new DateTime(1985, 5, 26), 35, "Rua Itaperuna", "150", "28030350", "Campos");
+    /*Employee jake = new Manager("Jake", "Blake", "jk@jk.com", new DateTime(1985, 5, 26), 35, "Rua Itaperuna", "150", "28030350", "Campos");
     jake.ShowAdress();
-    jake.GiveBonus();
+    jake.GiveBonus();*/
 // ---------- ↑↑↑↑ CREATING A USER TO APPLY POLIMORPHISM IN ONE OF THE METHODS (USING BASE TYPE REFFERENCE) ↑↑↑↑
 
 
 // ---------- ↓↓↓↓ CREATING A LIST OF EMPLOYEES AND LOOPING OVER IT TO SEE THE POLYMORPHISM EFFECT ↓↓↓↓
-    List<Employee> employessList = new List<Employee>(6) { bethany, george, mary, bobJunior, kevin, jake};
+    /*List<Employee> employessList = new List<Employee>(6) { bethany, george, mary, bobJunior, kevin, jake};
 
     foreach (Employee employee in employessList )
     {
         employee.DisplayEmployeeDetails();
         employee.GiveBonus();
-    }
+    }*/
 // ---------- ↑↑↑↑ CREATING A LIST OF EMPLOYEES AND LOOPING OVER IT TO SEE THE POLYMORPHISM EFFECT ↑↑↑↑
 
 
@@ -74,63 +139,63 @@ foreach (Employee e in employees)
 
 // ---------- ↓↓↓↓ START CREATING AN ARRAY OF EMPLOYEES IDS AND APPLYING METHODS ↓↓↓↓
 
-    /*Console.WriteLine("How many employees IDs do you want to register?");
-    int length = int.Parse(Console.ReadLine());
+/*Console.WriteLine("How many employees IDs do you want to register?");
+int length = int.Parse(Console.ReadLine());
 
-    int[] employeesId = new int[length];
+int[] employeesId = new int[length];
 
-    for (int i = 0; i < length; i++)
-    {
-        Console.WriteLine("Enter the employee ID: ");
-        int id = int.Parse(Console.ReadLine());
-        employeesId[i] = id;
-    }
+for (int i = 0; i < length; i++)
+{
+    Console.WriteLine("Enter the employee ID: ");
+    int id = int.Parse(Console.ReadLine());
+    employeesId[i] = id;
+}
 
-    for (int i = 0; i < employeesId.Length; i++)
-    {
-        Console.WriteLine($"ID {i + 1}: {employeesId[i]}");
-    }
+for (int i = 0; i < employeesId.Length; i++)
+{
+    Console.WriteLine($"ID {i + 1}: {employeesId[i]}");
+}
 
-    Array.Sort(employeesId);
+Array.Sort(employeesId);
 
-    for (int i = 0; i < employeesId.Length; i++)
-    {
-        Console.WriteLine($"ID {i + 1}: {employeesId[i]}");
-    }
+for (int i = 0; i < employeesId.Length; i++)
+{
+    Console.WriteLine($"ID {i + 1}: {employeesId[i]}");
+}
 
-    int[] employeeId2 = new int[length];
-    employeesId.CopyTo(employeeId2, 0);
+int[] employeeId2 = new int[length];
+employeesId.CopyTo(employeeId2, 0);
 
-    Array.Reverse(employeesId);
+Array.Reverse(employeesId);
 
-    Console.WriteLine("Employee 1 - IDs:");
-    for (int i = 0; i < employeesId.Length; i++)
-    {
-        Console.WriteLine($"ID {i + 1}: {employeesId[i]}");
-    }
-    Console.WriteLine("Employee 2 - IDs:");
-    for (int i = 0; i < employeesId.Length; i++)
-    {
-        Console.WriteLine($"ID {i + 1}: {employeeId2[i]}");
-    }*/
+Console.WriteLine("Employee 1 - IDs:");
+for (int i = 0; i < employeesId.Length; i++)
+{
+    Console.WriteLine($"ID {i + 1}: {employeesId[i]}");
+}
+Console.WriteLine("Employee 2 - IDs:");
+for (int i = 0; i < employeesId.Length; i++)
+{
+    Console.WriteLine($"ID {i + 1}: {employeeId2[i]}");
+}*/
 // ---------- ↑↑↑↑ END CREATING AN ARRAY OF EMPLOYEES IDS AND APPLYING METHODS ↑↑↑↑
 
 
 // ---------- ↓↓↓↓ START CREATING A LIST (COLLECTION) OF EMPLOYEES IDS ↓↓↓↓
-    /*List<int> employeeIdsList = new List<int>();
-    Console.WriteLine("Enter how many id you want to generate: ");
-    int idsCount = int.Parse(Console.ReadLine());
+/*List<int> employeeIdsList = new List<int>();
+Console.WriteLine("Enter how many id you want to generate: ");
+int idsCount = int.Parse(Console.ReadLine());
 
-    for (int i = 0; i < idsCount; i++)
-    {
-        Console.WriteLine("Enter the id: ");
-        employeeIdsList.Add(int.Parse(Console.ReadLine()));
-    }
-    Console.WriteLine("The current ids: ");
-    for (int i = 0; i < employeeIdsList.Count; i++)
-    {
-        Console.WriteLine($"{employees[i].FirstName} ID:\t{employeeIdsList[i]}");
-    }*/
+for (int i = 0; i < idsCount; i++)
+{
+    Console.WriteLine("Enter the id: ");
+    employeeIdsList.Add(int.Parse(Console.ReadLine()));
+}
+Console.WriteLine("The current ids: ");
+for (int i = 0; i < employeeIdsList.Count; i++)
+{
+    Console.WriteLine($"{employees[i].FirstName} ID:\t{employeeIdsList[i]}");
+}*/
 // ---------- ↑↑↑↑ END CREATING A LIST (COLLECTION) OF EMPLOYEES IDS ↑↑↑↑
 
 
@@ -181,3 +246,4 @@ foreach (Employee e in employees)
 
 //double bethWage = bethany.CalculateWage();
 //Console.WriteLine(bethWage);
+//Console.ReadLine();
---
+++ BettanyShopHRM/Utilities.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BettanyShopHRM.HR;
+
+namespace BettanyShopHRM
+{
+    public class Utilities
+    {
+        //using a fild as static we can use it directly from the class without the need to instantiate an object
+        private static string directory = @"C:\bettany_pie_shop\";
+        private static string fileName = @"employees.txt";
+
+        internal static void RegisterEmployee(List<Employee> employees)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Creating an Employee");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Which type of employee do you want to create?");
+            Console.ResetColor();
+            Console.WriteLine("1. Employee\n2. Manager\n3. Store Manager\n4. Researcher\n5. Junior Researcher");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Your selection: ");
+            Console.ResetColor();
+
+            string employeeType = Console.ReadLine();
+            int intEmployeeType = Int32.Parse(employeeType);
+
+            if (intEmployeeType < 0 || intEmployeeType > 5)
+            {
+                Console.WriteLine("Invalid employee type");
+                return;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Enter the first name: ");
+            Console.ResetColor();
+            string firstName = Console.ReadLine();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Enter last name: ");
+            Console.ResetColor();
+            string lastName = Console.ReadLine();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Enter email: ");
+            Console.ResetColor();
+            string email = Console.ReadLine();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Enter date of birth: ");
+            Console.ResetColor();
+            DateTime birthDay = DateTime.Parse(Console.ReadLine());
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Enter the hourly rate: ");
+            Console.ResetColor();
+            double hourlyRate = double.Parse(Console.ReadLine());
+
+            Employee employee = null;
+
+            switch(intEmployeeType)
+            {
+                case 1:
+                    employee = new Employee(firstName, lastName, email, birthDay, hourlyRate);
+                    break;
+                case 2:
+                    employee = new Manager(firstName, lastName, email, birthDay, hourlyRate);
+                    break;
+                case 3:
+                    employee = new StoreManager(firstName, lastName, email, birthDay, hourlyRate);
+                    break;
+                case 4:
+                    employee = new Researcher(firstName, lastName, email, birthDay, hourlyRate);
+                    break;
+                case 5:
+                    employee = new JuniorResearcher(firstName, lastName, email, birthDay, hourlyRate);
+                    break;
+            }
+
+            employees.Add(employee);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Employee created!");
+            Console.ResetColor();
+        }
+
+        internal static void CheckForExistingEmployeeFile()
+        {
+            string path = $"{directory}{fileName}";
+            bool existingFileFound = File.Exists(path);
+
+            if (existingFileFound)
+            {
+                Console.WriteLine("An existing file with employee data is found!"); 
+            }
+            else
+            {
+                if (!Directory.Exists(directory))
+                    Directory.CreateDirectory(directory);
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Directory is ready for save data.");
+                Console.ResetColor();
+            }
+        }
+
+        internal static void ViewAllEmployees(List<Employee> employees)
+        {
+            foreach (Employee employee in employees)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                employee.DisplayEmployeeDetails();
+                Console.ResetColor();
+            }
+        }
+
+        internal static void LoadEmployees(List<Employee> employees)
+        {
+            string path = $"{directory}{fileName}";
+            if (File.Exists(path))
+            {
+                employees.Clear();
+
+                string[] employeesAsString = File.ReadAllLines(path);
+                for (int i = 0; i < employeesAsString.Length; i++)
+                {
+                    string[] employeeSplit = employeesAsString[0].Split(";");
+                    string firstName = employeeSplit[0].Substring(employeeSplit[0].IndexOf(":") + 1);
+                    string lastName = employeeSplit[1].Substring(employeeSplit[1].IndexOf(":") + 1);
+                    string email = employeeSplit[2].Substring(employeeSplit[2].IndexOf(":") + 1);
+                    DateTime birthDay = DateTime.Parse(employeeSplit[3].Substring(employeeSplit[3].IndexOf(":") + 1));
+                    double hourlyRate = double.Parse(employeeSplit[4].Substring(employeeSplit[4].IndexOf(":") + 1));
+                    string employeeType = employeeSplit[5].Substring(employeeSplit[5].IndexOf(":") + 1);
+
+                    Employee employee = null;
+
+                    switch (employeeType)
+                    {
+                        case "1":
+                            employee = new Employee(firstName, lastName, email, birthDay, hourlyRate);
+                            break;
+                        case "2":
+                            employee = new Manager(firstName, lastName, email, birthDay, hourlyRate);
+                            break;
+                        case "3":
+                            employee = new StoreManager(firstName, lastName, email, birthDay, hourlyRate);
+                            break;
+                        case "4":
+                            employee = new Researcher(firstName, lastName, email, birthDay, hourlyRate);
+                            break;
+                        case "5":
+                            employee = new JuniorResearcher(firstName, lastName, email, birthDay, hourlyRate);
+                            break;
+                    }
+
+                    employees.Add(employee);
+
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    //Console.WriteLine($"Loaded {employees.Count} employees!");
+                    Console.ResetColor();
+                }
+            }
+
+        }
+
+        internal static void SaveEmployee(List<Employee> employees)
+        {
+            string path = $"{directory}{fileName}";
+            StringBuilder sb = new StringBuilder();
+            foreach (Employee employee in employees)
+            {
+                string type = GetEmployeeType(employee);
+                sb.Append($"firstName:{employee.FirstName};");
+                sb.Append($"lastName:{employee.LastName};");
+                sb.Append($"email:{employee.Email};");
+                sb.Append($"birthDay:{employee.BirthDay.ToShortDateString()};");
+                sb.Append($"hourlyRate:{employee.HourlyRate};");
+                sb.Append($"Employee type:{type};");
+
+                sb.Append(Environment.NewLine);
+            }
+            File.WriteAllText(path, sb.ToString());
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("The employee data has beem saved!");
+            Console.ResetColor();
+        }
+
+        private static string GetEmployeeType(Employee employee)
+        {
+            if (employee is Manager)
+                return "2";
+            else if (employee is StoreManager)
+                return "3";
+            else if (employee is Researcher)
+                return "4";
+            else if (employee is JuniorResearcher)
+                return "5";
+            else if (employee is Employee)
+                return "1";
+            return "0";
+        }
+
+        //internal static string TypeOfEmployee(string employeeType)
+        //{
+        //    string formatedEmployeeType = employeeType.IndexOf(()
+        //    return employeeType;
+        //}
+    }
+}
